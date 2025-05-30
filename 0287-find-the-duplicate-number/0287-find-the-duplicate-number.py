8c9d80b230330a3376127d0e4bdaf505b25d16f4
Suril Jignesh Purohit <45599204+SurilPurohit@users.noreply.github.com>
2025-05-30 19:27:09 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,7 +1,17 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        d = {}
-        for i in nums:
-            d[i] = d.get(i, 0) + 1
-            if d[i] > 1:
-                return i
+        # d = {}
+        # for i in nums:
+        #     d[i] = d.get(i, 0) + 1
+        #     if d[i] > 1:
+        #         return i
+
+        # two pointer
+        slow = nums[0]
+        fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                return slow
+        
